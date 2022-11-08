8b2029de5916172196c16cfb659c4a58f804d3dc
Kerally <110418322+Kerally@users.noreply.github.com>
2022-11-08 02:38:12 +0200

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,106 +1,77 @@
+from pprint import pprint
 import datetime
 import uuid
-import pprint
-from decimal import Decimal
 
 
 class Bank:
-    UUID = uuid.uuid4()
-    First_name = 'Vladyslav'
-    Last_name = 'Sadullaiev'
-    now = datetime.datetime.now()
-    data = now.strftime(('%Y-%m-%d %H:%M'))
     value = 1000
     value_deposit = 0
     list_of_transaction = []
+    now = datetime.datetime.now()
+    data = now.strftime(('%Y-%m-%d_%H:%M'))
 
+    def __init__(self, amount_of_output, amount_to_deposit):
+        self.UUID = uuid.uuid4()
+        self.First_name = 'Vladyslav'
+        self.Last_name = 'Sadullaiev'
 
-    def __init__(self, amount_to_output, amount_to_deposit):
-        self.amount_to_output = amount_to_output
+        self.amount_of_output = amount_of_output
         self.amount_to_deposit = amount_to_deposit
 
 
-    def get_value(self):
-        return "Money: {}, Deposit: {}".format(self.value, self.value_deposit)
+    # return value of money, deposits
+    def get_value():
+        return ("Money: {} | Deposit: {}").format(Bank.value, Bank.value_deposit)
 
 
+    # output cash from money
     def output(self):
-        # проверка на возможность снять деньги
-        if Bank.value - Decimal(self.amount_to_output) < 0:
+        # test for correct input (int/float)
+        try: float(self.amount_of_output)
+        except: 
+            try: int(self.amount_of_output)
+            except: self.amount_of_output = 100
+
+        # test for enought money to output
+        if Bank.value - self.amount_of_output < 0:
             return "Not enought money"
         else:
-            Bank.value = Bank.value - Decimal(self.amount_to_output)
-            # заполняем масив транзакций транзакцией Output money
-            transaction = "{}, Output money: {}".format(Bank.data, self.amount_to_output)
+            Bank.value = Bank.value - self.amount_of_output
+            # add transaction "output money" to transaction list
+            transaction = ("{}: Output money: {}").format(Bank.data, self.amount_of_output)
             Bank.list_of_transaction.append(transaction)
-            return "Operation [Output money] was successful!"
+            return "Operation [output money] was successful"
 
 
     def deposit(self):
-        # проверка на возможность положить деньги под депозит
-        if Bank.value - Decimal(self.amount_to_deposit) < 0:
+        # test for correct input (int/float)
+        try: float(self.amount_to_deposit)
+        except: 
+            try: int(self.amount_to_deposit)
+            except: self.amount_to_deposit = 100
+
+        # test for enought money to make deposit
+        if Bank.value - self.amount_to_deposit < 0:
             return "Not enought money"
-        else:   
-            Bank.value_deposit = Decimal(Bank.value_deposit) + Decimal(self.amount_to_deposit)
-            Bank.value = Bank.value - Decimal(self.amount_to_deposit)
-            # заполняем масив транзакций транзакцией Invest to deposit
-            transaction = "{}, Invest to deposit: {}".format(Bank.data, self.amount_to_deposit)
+        else:
+            Bank.value_deposit = Bank.value_deposit + self.amount_to_deposit
+            Bank.value = Bank.value - self.amount_to_deposit
+            # add transaction "made a deposit" to transaction list
+            transaction = ("{}: Made a deposit: {}").format(Bank.data, self.amount_to_deposit)
             Bank.list_of_transaction.append(transaction)
-            return "Operation [Invest to deposit] was successful!"
+            return "Operation [make a deposit] was successful"
 
-
-    def transaction(self):
-        return self.list_of_transaction
+    
+    # get list of transaction
+    def get_transaction():
+        return Bank.list_of_transaction
 
 
 if __name__ == '__main__':
-    Operation1 = Bank(100, 300)
-    print(Operation1.output())
-    print(Operation1.deposit())
-    pprint.pprint(Operation1.transaction())
-
-    Operation2 = Bank(150, 200)
-    print(Operation2.output())
-    print(Operation2.deposit())
-
-    pprint.pprint(Operation2.transaction())
-    print(Operation2.get_value())
-
-
-
-
-
-
-#-----------------------------------------------------------------------
-# меню программы
-# def main():
-#     print("""
-#     1 - Amount of money
-#     2 - Output money
-#     3 - Deposit
-#     4 - Transaction
-#     5 - End
-#     """)
-#     choise = input("Enter the number: ")
-#     match choise:
-#         case '1':
-#             print(Bank.get_value(Bank.value, Bank.value_deposit))
-#         case '2':
-#             print(Bank.output(Bank.amount_of_outpto))
-#         case '3':
-#             print(Bank.deposit(Bank.amount_to_deposit))
-#         case '4':
-#             pprint.pprint(Bank.transaction(Bank.list_of_transaction))
-#         case '5':
-#             quit()
-#         case _:
-#             print("Enter the number again (1-5)")
-#             main()
-#-----------------------------------------------------------------------
-# amount_of_outpto = input("Enter the amount to output(defoult: 100): ")
-# проверка на число
-# try: float(amount_of_outpto)
-# except: 
-#     try: int(amount_of_outpto)
-#     except: amount_of_outpto = 100
+    print(Bank.get_value())
+    transaction1 = Bank(234, 123.45)
+    print(transaction1.output())
+    print(transaction1.deposit())
+    pprint(Bank.get_transaction())
+    print(Bank.get_value())
 
